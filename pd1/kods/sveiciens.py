79ee0a7bb2ea4625e42397835f7064d152adca70
do317 <246140219+do317@users.noreply.github.com>
2026-09-23 12:54:32 +0300

    komenti, labojumi

--- pd1/kods/sveiciens.py
+++ pd1/kods/sveiciens.py
@@ -1,7 +1,6 @@
 lietotajvards = "do317"
-kratuve = "programmesana-pamatkurss"
-saite = f"https://github.com/{lietotajvards}/{kratuve}"
-# print(saite)
+
+#2 līnijas
 print(lietotajvards)
-print("Programmēšanas Pamatkurss")
-# f formatē {} blokus
+#Kursa vārds
+print("Programmēšanas Pamatkurss - Ievads programmēšanā un darba vide")
--- saite.py
+++ saite.py
@@ -1,5 +1,5 @@
 lietotajvards = "do317"
 kratuve = "programmesana-pamatkurss"
+# f"" formatē {} blokus
 saite = f"https://github.com/{lietotajvards}/{kratuve}"
 print(saite)
-# f formatē {} blokus
